45030d5e4839aab9ae2c90db5086336e1def0a91
Rater1 <pjuske.10@live.dk>
2023-02-28 10:28:55 +0100

    jonas er gud

--- src/rps/bll/player/Player.java
+++ src/rps/bll/player/Player.java
@@ -48,18 +48,42 @@ public class Player implements IPlayer {
      */
     @Override
     public Move doMove(IGameState state) {
-        //Historic data to analyze and decide next move...
         ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
 
-        Random randomMove = new Random();
-        int nextMove = randomMove.nextInt(3);
-        if (nextMove == 0) {
-            return Move.Rock;
-        } else if (nextMove == 1) {
-            return Move.Paper;
+        // Count the number of times the human player has chosen each move in the last two rounds
+        int rockCount = 0;
+        int paperCount = 0;
+        int scissorCount = 0;
+        for (int i = results.size() - 1; i >= Math.max(0, results.size() - 2); i--) {
+            Result result = results.get(i);
+            if (result.getLoserMove().equals("Rock")) {
+                rockCount++;
+            } else if (result.getLoserMove().equals("Paper")) {
+                paperCount++;
+            } else if (result.getLoserMove().equals("Scissor")) {
+                scissorCount++;
+            }
         }
-        else{
+
+        // Use the human player's move history to make a more informed decision
+        if (rockCount >= 2) {
+            return Move.Paper;
+        } else if (paperCount >= 2) {
             return Move.Scissor;
+        } else if (scissorCount >= 2) {
+            return Move.Rock;
+        } else {
+            // Choose a move randomly with equal probability
+            Random random = new Random();
+            int randomMove = random.nextInt(3);
+            if (randomMove == 0) {
+                return Move.Rock;
+            } else if (randomMove == 1) {
+                return Move.Paper;
+            } else {
+                return Move.Scissor;
+            }
         }
     }
 }
+
