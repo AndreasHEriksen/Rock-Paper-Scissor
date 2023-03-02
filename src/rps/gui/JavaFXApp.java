d67b81fe372948dcce858498b97c36ec1ad0d2c7
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-03-02 13:01:44 +0100

    h

--- src/rps/gui/JavaFXApp.java
+++ src/rps/gui/JavaFXApp.java
@@ -5,7 +5,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.logging.Logger;
+
 
 /**
  * JavaFX implementation of the RPS game
@@ -20,8 +26,9 @@ public class JavaFXApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
-        Parent root = FXMLLoader.load(getClass().getResource("/rps/gui/view/GameView.fxml"));
+        Font customFont = Font.loadFont("view/font/Minecraftia.ttf", 12);
+        Parent root = FXMLLoader.load(getClass().getResource("view/GameView.fxml"));
+        root.getStylesheets().add(getClass().getResource("/rps/gui/view/GameViewCSS.css").toExternalForm());
         stage.setTitle("Welcome to the not-implemented Rock-Paper-Scissor game!");
         stage.setScene(new Scene(root));
         stage.show();
